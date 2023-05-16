3666597bdbb8de721ff8be31fae45a7f9a48bd23
elizacaraman <>
2023-05-16 15:51:17 -0700

    Update booktable.sql

--- booktable.sql
+++ booktable.sql
@@ -1,5 +1,18 @@
 USE [BookMall]
 GO
+select *from __MigrationHistory
+select *from PDbTables
+select *from UDbTables
+
+UPDATE PDbTables
+SET Author='Meadow Murphy'
+WHERE Title='Kiss and Cry';
+
+
+UPDATE UDbTables
+SET Level= 0 
+WHERE Username like'%eliza%';
+
 SET IDENTITY_INSERT [dbo].[PDbTables] ON 
 GO
 INSERT [dbo].[PDbTables] ([Id], [OwnerId], [Author], [Title], [Description], [Genre], [ImageUrl], [PdfFile], [Price], [Pages], [ISBN], [JpgFile], [PdfUrl]) VALUES (1, 1, N'Meadow Murphy', N'Kiss and Cry', NULL, N'Romantique', N'~/Content/Covers/3C66F948D67E0B0D386908E5A038F6E0.jpg', N'', 350, 168, N'9786064400642', N'', N'~/Content/Books/3C66F948D67E0B0D386908E5A038F6E0.pdf')
